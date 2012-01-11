a85ae421b9f0ec6a14789b6d593e25de41297564
Ben <mastahyeti@gmail.com>
2012-01-11 16:35:16 -0600

    working audio

--- pystrobe.py
+++ pystrobe.py
@@ -3,9 +3,44 @@
 # MIT license
 # YAY CURSES... just kidding
 
-import curses
 from time import sleep
 from sys import argv
+from math import sin,pi
+from multiprocessing import Process, Pipe
+import curses
+import pyaudio
+import struct
+
+def play_binaural(left_freq=440, right_freq=460):
+    #set some vars
+    sampwidth   = 2
+    frate       = 44100.0
+    amp         = 8000.0
+    channels    = 2
+    periods     = 20
+    data_size = int((frate/left_freq)*(frate/right_freq)) * periods
+    audio_string = ""
+
+    #setup pyaudio and stream
+    PyAudio = pyaudio.PyAudio
+    p = PyAudio()
+    stream = p.open(format=p.get_format_from_width(sampwidth),rate=int(frate),output=True,channels=channels)
+
+    #generate audio data
+    for x in range(data_size):
+        left = sin(2*pi*left_freq*(x/frate))
+        right = sin(2*pi*right_freq*(x/frate))
+        audio_string += struct.pack("hh",left*amp/2,right*amp/2)
+
+    print "playing"
+
+    while True:
+	    stream.write(audio_string)
+
+    #cleanup
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 def strobe(screen,frequency,cp_1,cp_2):
 	#this is a generator (don't care about what it generates) that
@@ -15,24 +50,35 @@ def strobe(screen,frequency,cp_1,cp_2):
 	sleep_time = 1/frequency
 	cps = [cp_1,cp_2]
 	i = 0
-	while 1:
-		c = screen.getch()
-		if c != -1:
-			if c == curses.KEY_UP:
-				frequency += frequency/20
-				sleep_time = 1/frequency
-			if c == curses.KEY_DOWN:
-				frequency -= frequency/20
-				sleep_time = 1/frequency
 
-		screen.bkgd(' ',curses.color_pair(cps[i%2]))
-		screen.move(0,0)
-		screen.clrtoeol()
-		screen.addstr("frequency: %f hz\t\t\tuse keyboad arrows to adjust frequency(5 percent at a time)" % frequency)
-		screen.refresh()
-		i ^= 1
-		sleep(sleep_time)
-		yield 1	 
+	try:
+		child = Process(target=play_binaural,args=(200,frequency+200))
+		child.start()
+
+		while 1:
+			c = screen.getch()
+			if c != -1:
+
+				if c == curses.KEY_UP:
+					frequency += frequency/20
+					sleep_time = 1/frequency
+				if c == curses.KEY_DOWN:
+					frequency -= frequency/20
+					sleep_time = 1/frequency
+				child.terminate()
+				child = Process(target=play_binaural,args=(200,frequency+200))
+				child.start()
+			screen.bkgd(' ',curses.color_pair(cps[i%2]))
+			screen.move(0,0)
+			screen.clrtoeol()
+			screen.addstr("frequency: %f hz\t\t\tuse keyboad arrows to adjust frequency(5 percent at a time)" % frequency)
+			screen.refresh()
+			i ^= 1
+			sleep(sleep_time)
+			yield 1	
+	except:
+		child.terminate()
+		raise
 
 def main(screen):
 	try:
@@ -42,9 +88,11 @@ def main(screen):
 		except:
 			print "usage: python ./pystrobe.py <frequency>"
 			die_nice(screen)
+
 		#setup some color pairs
 		curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_BLACK)
 		curses.init_pair(2,curses.COLOR_BLACK,curses.COLOR_WHITE)
+
 		#FREAK OUT!
 		for i in strobe(screen,frequency,1,2):
 			pass
